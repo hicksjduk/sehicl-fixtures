608ae0fb775954a8d2ce80bf139145a892e15fa3
Jeremy Hicks <jeremy@thehickses.org.uk>
2017-06-28 16:34:54 +0100

    Fixture set combinations

--- src/main/java/uk/org/sehicl/fixtures/FixtureSet.java
+++ src/main/java/uk/org/sehicl/fixtures/FixtureSet.java
@@ -4,15 +4,58 @@ import java.io.PrintWriter;
 import java.io.StringWriter;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Iterator;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.stream.IntStream;
 
-public class FixtureSet
+public class FixtureSet implements Iterator<List<Match>>
 {
     private final List<Match> matches;
 
+    private final int maxCombinations;
+    private int nextCombination = 0;
+
     public FixtureSet(Collection<Match> matches)
     {
+        this(matches, IntStream.rangeClosed(3, matches.size()).reduce(2, (x, y) -> x * y));
+    }
+
+    public FixtureSet(Collection<Match> matches, int combination)
+    {
         this.matches = new ArrayList<>(matches);
+        maxCombinations = IntStream.rangeClosed(3, matches.size()).reduce(2, (x, y) -> x * y);
+    }
+    
+    @Override
+    public boolean hasNext()
+    {
+        return nextCombination < maxCombinations;
+    }
+
+    @Override
+    public List<Match> next()
+    {
+        if (!hasNext())
+        {
+            throw new IllegalStateException();
+        }
+        return getCombination(nextCombination++);
+    }
+
+    private List<Match> getCombination(int combination)
+    {
+        List<Match> workingSet = new ArrayList<>(matches);
+        int workingCombination = combination;
+        List<Match> answer = new LinkedList<>();
+        while (!workingSet.isEmpty())
+        {
+            int workingSetSize = workingSet.size();
+            int i = workingCombination % workingSetSize;
+            workingCombination = workingCombination / workingSetSize;
+            answer.add(workingSet.remove(i));
+        }
+        return answer;
     }
 
     public String toString()
---
+++ src/test/java/uk/org/sehicl/fixtures/FixtureSetTest.java
@@ -0,0 +1,29 @@
+package uk.org.sehicl.fixtures;
+
+import static org.assertj.core.api.Assertions.*;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+import org.junit.Test;
+
+public class FixtureSetTest
+{
+    @Test
+    public void test()
+    {
+        AtomicInteger team = new AtomicInteger();
+        int matchCount = 5;
+        FixtureSet fs = new FixtureSet(
+                IntStream
+                        .range(0, matchCount)
+                        .mapToObj(i -> new Match(new Team("" + team.incrementAndGet()),
+                                new Team("" + team.incrementAndGet())))
+                        .collect(Collectors.toList()));
+        Set<String> permutations = new HashSet<>();
+        fs.forEachRemaining(p -> assertThat(permutations.add(p.toString())).as(p.toString()).isTrue());
+    }
+
+}
