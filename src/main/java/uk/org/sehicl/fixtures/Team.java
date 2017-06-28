f52b73bfd5a078148a1fd1aa0894648009aba511
Jeremy Hicks <jeremy@thehickses.org.uk>
2017-06-28 08:54:08 +0100

    Team class

---
+++ src/main/java/uk/org/sehicl/fixtures/Team.java
@@ -0,0 +1,16 @@
+package uk.org.sehicl.fixtures;
+
+public class Team
+{
+    private final String name;
+
+    public Team(String name)
+    {
+        this.name = name;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+}
